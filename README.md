2ce1bbd39455a390acb527c62a4bb271265ee817
Michael Myers <myersmyer@gmail.com>
2018-05-18 00:21:47 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bamazon
 MySql storefront exercise
+
+## Purpose and creation of app
+This is a CLI app that uses sql, inquirer, and console.table to create a webstore style purchase experience. It was created mid-May of 2018. I created a database and populated a table inside of Sequel Pro with some dummy product data. When the app is run the results will display the contents of the database (the 'inventory') and prompt the user to make a purchase. 
+
+### Making a purchase
+Using inquirer the app will prompt the user to select an item id. If the item id does not exist the user will be alerted and the inventory will be redisplayed with the prompt to select an item id. Once the user selects an appropriate item id the app will prompt the user for a quantity. If the quanity is higher than current stock levels the user will receive an alert. If the quantity is within current stock levels the user will receive a prompt letting them know how much their transaction cost. 
+
+### Future development
+I was a bit pressed for time in making this so in the future I would like to add some functionality to this. As of now there is just a customer level experience. I would like to add manager and supervisor level experiences. The manager level would be able to add to inventory levels and add new products. The supervisor level would be able to create entire departments, view cost overheads, and calculate the most profitable departments based on sales and overheads. 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+require('console.table');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -8,8 +10,84 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+connection.connect(function (err) {
+    if (err) {
+        console.log('connection error' + err.stack);
+    }
+    startApp();
+});
+
+function startApp() {
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        pickItem(res);
+    });
+}
+
+function pickItem(inventory) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'choice',
+            message: 'Type the ID of the item you want to purchase',
+            validate: function (val) {
+                return !isNaN(val);
+            }
+        }
+    ])
+        .then(function (val) {
+            var pickId = parseInt(val.choice);
+            var product = scanInventory(pickId, inventory);
+            if (product) {
+                pickAmount(product);
+            }
+            else {
+                console.log("\nSorry. We don't carry that.\n");
+                startApp();
+            }
+        });
+}
+
+function pickAmount(product) {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'amount',
+            message: 'How many many?',
+            validate: function (val) {
+                return val > 0;
+            }
+        }
+    ])
+        .then(function (val) {
+            var amount = parseInt(val.amount);
+            if (amount > product.quantity) {
+                console.log('Sorry. Insufficient quantity!');
+                startApp();
+            }
+            else {
+                buyItem(product, amount);
+            }
+        });
+}
+
+function buyItem(product, amount) {
+    connection.query(
+        'UPDATE products SET quantity = quantity - ? WHERE item_id = ?',
+        [amount, product.item_id],
+        function (err, res) {
+            console.log('\nThanks. Your total cost is $' + amount * product.price + '\n');
+            startApp();
+        }
+    );
+}
+
+function scanInventory(pickId, inventory) {
+    for (var i = 0; i < inventory.length; i++) {
+        if (inventory[i].item_id === pickId) {
+            return inventory[i];
+        }
+    }
+    return null;
+}
