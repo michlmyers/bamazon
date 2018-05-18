3b46c53399c5b87f142fa24bd3c7011fa91cf7f6
Michael Myers <myersmyer@gmail.com>
2018-05-17 20:47:10 -0400

    adding to git

---
+++ bamazonSeeds.sql
@@ -0,0 +1,31 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Bjork LP","Vinyl Store",25,350);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Vater Drumsticks","Instruments",10,1000);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Beach House LP","Vinyl Store",30,100);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Mr. Chow Dog Food","Pet Store",30,3000);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Almond Milk","Groceries",2,10000);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Peanut Butter Cliff Bars","Groceries",3,4000);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Uncured Bacon","Groceries",5,1000);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Descendants LP","Vinyl Store",20,50);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Caramel Ice Cream","Groceries",5,400);
+
+INSERT INTO products (product_name, department_name, price, quantity)
+VALUES ("Doggie Toothpaste","Pet Store",6,1000);
